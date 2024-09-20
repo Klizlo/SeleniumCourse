07024467fa273df16f7ade6cd44aae88a344d814
Klizlo <klizlo0111@gmail.com>
2024-09-20 14:13:12 +0200

    The basic task to search for "Lublin" in Google search engine

--- src/main/java/pollub/Main.java
+++ src/main/java/pollub/Main.java
@@ -1,7 +1,23 @@
 package pollub;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        // Disable search engine choice screen
+        options.addArguments("--disable-search-engine-choice-screen");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.pl");
+        Thread.sleep(1000); // Sleep 1 second
+        driver.findElement(By.id("L2AGLb")) // Find button to accept all cookies
+                        .click();
+        driver.findElement(By.className("gLFyf")) // Find search bar
+                .sendKeys("Lublin"); // Type "Lublin" in search bar
+        Thread.sleep(1000); // Sleep 1 second
+        driver.findElement(By.className("gNO89b")) // Find search button
+                .click();
     }
 }
